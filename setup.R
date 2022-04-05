9e08129123a2d0d3fdb86e8f0924f0ad1bb12b87
Omi Johnson <ojohnson@bigelow.org>
2022-04-05 17:07:19 -0400

    finished recipes & parsnip pages

--- setup.R
+++ setup.R
@@ -1,9 +1,37 @@
 library(tidymodels)
+library(ranger)
 
 # defining data_iris
+
 data_iris <- bind_cols(all_of(iris), 
                        row = 1:nrow(iris))
 
+# RSample - generate data splits
+
 data_split <- initial_split(data_iris, prop = 3/4)
+
+# Recipes - define and apply preprocessing steps
+
+iris_recipe_prepped <- recipe(Species ~ ., 
+                           data = training(data_split)) |>
+  update_role(row, new_role = "ID") |>
+  step_corr(all_numeric_predictors()) |>
+  step_normalize(all_numeric_predictors()) |>
+  step_rename(Row = row) |>
+  prep()
+
+prepped_training <- juice(iris_recipe_prepped)
+prepped_testing <- bake(iris_recipe_prepped, testing(data_split))
+
+#  Parsnip - create a model and generate predictions
+
+rf_fit <- rand_forest() |>
+  set_mode("classification") |>
+  set_args(trees = 500) |>
+  set_engine("ranger") |>
+  fit(Species ~ ., data = prepped_training)
+
+predictions <- augment(rf_fit, prepped_testing)
+
 
 
